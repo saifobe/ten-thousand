1eb4b96952b66a6efa0e67cf9f75c826dd9c9fc4
saifobe <obeidatsaif93@gmail.com>
2023-04-17 14:42:07 +0000

    Lab 06 done

---
+++ chatGPT.md
@@ -0,0 +1,86 @@
+My chatGBT Request 
+```
+if new_counter[1] == 3:
+            unbanked_points += 1000
+        if new_counter[2] == 3:
+            unbanked_points += 200
+        if new_counter[3] == 3:
+            unbanked_points += 300
+        if new_counter[4] == 3:
+            unbanked_points += 400
+        if new_counter[5] == 3:
+            unbanked_points += 500
+        if new_counter[6] == 3:
+            unbanked_points += 600 convert this python code to for loop
+```
+
+chatGPT response:
+```python
+for i in range(2, 7):
+    if new_counter[i] == 3:
+        unbanked_points += i * 100
+```
+
+### on chatGPT I've put the error that ouccured to ous
+My chatGBT Request
+```python
+test_input = (2, 2, 3, 3, 6, 6), expected = 1500
+
+    @pytest.mark.parametrize(
+        "test_input,expected",
+        [
+            (tuple(), 0),
+            ((1,), 100),
+            ((1, 1), 200),
+            ((1, 1, 1), 1000),
+            ((1, 1, 1, 1), 2000),
+            ((1, 1, 1, 1, 1), 4000),
+            ((1, 1, 1, 1, 1, 1), 8000),
+            ((2,), 0),
+            ((2, 2), 0),
+            ((2, 2, 2), 200),
+            ((2, 2, 2, 2), 400),
+            ((2, 2, 2, 2, 2), 800),
+            ((2, 2, 2, 2, 2, 2), 1600),
+            ((3,), 0),
+            ((3, 3), 0),
+            ((3, 3, 3), 300),
+            ((3, 3, 3, 3), 600),
+            ((3, 3, 3, 3, 3), 1200),
+            ((3, 3, 3, 3, 3, 3), 2400),
+            ((4,), 0),
+            ((4, 4), 0),
+            ((4, 4, 4), 400),
+            ((4, 4, 4, 4), 800),
+            ((4, 4, 4, 4, 4), 1600),
+            ((4, 4, 4, 4, 4, 4), 3200),
+            ((5,), 50),
+            ((5, 5), 100),
+            ((5, 5, 5), 500),
+            ((5, 5, 5, 5), 1000),
+            ((5, 5, 5, 5, 5), 2000),
+            ((5, 5, 5, 5, 5, 5), 4000),
+            ((6,), 0),
+            ((6, 6), 0),
+            ((6, 6, 6), 600),
+            ((6, 6, 6, 6), 1200),
+            ((6, 6, 6, 6, 6), 2400),
+            ((6, 6, 6, 6, 6, 6), 4800),
+            ((1, 2, 3, 4, 5, 6), 2000),
+            ((2, 2, 3, 3, 4, 6), 0),
+            ((2, 2, 3, 3, 6, 6), 1500),
+            ((1, 1, 1, 2, 2, 2), 2400),
+        ],
+    )
+    def test_all(test_input, expected):
+        actual = GameLogic().calculate_score(test_input)
+>       assert actual == expected
+E       assert 0 == 1500
+
+tests/test_calculate_score.py:145: AssertionError
+```
+chatGPT response:
+```python
+if len(new_counter) == 3 and all(value == 2 for value in new_counter.values()):
+            unbanked_points += 1500
+```
---
+++ ten_thousand/game_logic.py
@@ -0,0 +1,92 @@
+import random
+from collections import Counter
+
+class GameLogic():
+    def __init__(self):
+        pass
+    @classmethod
+    def calculate_score(cls,tupleroll):
+        """
+        A class for calculating the score of a given roll in a dice game.
+        calculate_score(cls, tupleroll): Calculates the score of a given roll and returns it as an integer
+
+        """
+        unbanked_points = 0
+        new_counter = Counter(tupleroll)
+
+        if new_counter[1] >= 1 and new_counter[1] < 3:
+            unbanked_points += 100 * new_counter[1]
+
+        if new_counter[5] >= 1 and new_counter[5] < 3:
+            unbanked_points += 50 * new_counter[5]
+
+        if new_counter[1] == 1 and new_counter[2] == 1 and new_counter[3] == 1 and new_counter[4] == 1 and new_counter[5] == 1 and new_counter[6] == 1:
+            return 2000
+        
+        
+
+        if new_counter[1] == 3:
+            unbanked_points += 1000
+        for i in range(2, 7):
+            if new_counter[i] == 3:
+                unbanked_points += i * 100
+        
+
+        if new_counter[1] == 4:
+            unbanked_points += 2000
+        # if new_counter[2] == 4:
+        #     unbanked_points += 400
+        # if new_counter[3] == 4:
+        #     unbanked_points += 600
+        # if new_counter[4] == 4:
+        #     unbanked_points += 800
+        # if new_counter[5] == 4:
+        #     unbanked_points += 1000
+        # if new_counter[6] == 4:
+        #     unbanked_points += 1200
+        for i in range(2, 7):
+            if new_counter[i] == 4:
+                unbanked_points += i * 200
+
+        if new_counter[1] == 5:
+            unbanked_points += 4000
+        for i in range(2, 7):
+            if new_counter[i] == 5:
+                unbanked_points += i * 400
+
+        if new_counter[1] == 6:
+            unbanked_points += 8000
+        for i in range(2, 7):
+            if new_counter[i] == 6:
+                unbanked_points += i * 800
+
+        
+        if len(new_counter) == 3 and all(value == 2 for value in new_counter.values()):
+            unbanked_points += 1500
+
+        if len(new_counter) == 2 and len(set(new_counter.values())) == 1 and list(set(new_counter.values()))[0] == 3:
+            unbanked_points = unbanked_points*2
+           
+        
+        
+        return unbanked_points
+    
+    @staticmethod
+    def roll_dice(num_dice):
+        """
+        A static method for rolling a given number of dice.
+        roll_dice(num_dice): Rolls a given number of dice and returns the result as a tuple of integers
+
+        """
+        return tuple(random.randint(1, 6) for _ in range(num_dice))
+    
+
+
+if __name__ == "__main__":
+    
+    game = GameLogic()
+    print(game.roll_dice(6))
+    print(game.calculate_score((5,)))  # 50
+
+
+
